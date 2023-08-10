bc615fd8ecb27294d8323eaed8399b594b7af528
Divya <divya.kolora@gmail.com>
2023-08-10 14:38:50 -0600

    added anchor tags

--- views/Index.jsx
+++ views/Index.jsx
@@ -1,29 +1,27 @@
-  
 //   class MyFirstComponent extends React.Component() {
 //   return (
 //     <div style={myStyle}>My First React Component!</div>;
 //   }
 //   }
 
-import React from 'react';
+import React from "react";
 
 const myStyle = {
-  color: '#ffffff',
-  backgroundColor: '#000000',
-  };
+  color: "#ffffff",
+  backgroundColor: "#000000",
+};
 
-  export default  function Index(){
-    return(
-      <div style={myStyle}>
-        <h1>See all the pokemon!</h1>
-        <ul>
-          {pokemon.map((poke)=><li>{poke.name[0].toUppercase().poke.name.slice(1)}</li>)}
-        </ul>
-      </div>
-    )
-  }
-
-
-
-
-  
+export default function Index() {
+  return (
+    <div style={myStyle}>
+      <h1>See all the pokemon!</h1>
+      <ul>
+        {pokemon.map((poke, index) => (
+          <li>
+            a href={"/${index}"}>{poke.name[0].toUppercase().poke.name.slice(1)}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
